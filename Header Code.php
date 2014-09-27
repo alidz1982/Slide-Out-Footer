07fffded40b508603f26d0850da8919f8d7bc8e7
alidz1982 <alidz1982@users.noreply.github.com>
2014-09-27 19:23:52 +0100

    Create Header Code.php

---
+++ Header Code.php
@@ -0,0 +1,15 @@
+<link href="http://fonts.googleapis.com/css?family=PT+Sans+Narrow:700" rel="stylesheet">
+  <link href="http://demo.tutorialzine.com/2013/08/slideout-footer-css/assets/css/style.css" rel="stylesheet">
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="http://myweeblytricksdemo.weebly.com/files/theme/slideoutfooter.css">
+    <style>
+footer a.logo:before{
+ content: '';
+ display: inline-block;
+ background: url('http://myweeblytricksdemo.weebly.com/files/theme/mylogomwt.png') no-repeat ;
+ width: 50px;
+ height: 37px;
+ vertical-align: text-top;
+  margin-left:-10%;
+}
+</style>
