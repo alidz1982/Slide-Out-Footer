cb0e3636666acd01cc677fdf19eaf187ccfaac1c
alidz1982 <alidz1982@users.noreply.github.com>
2014-09-27 19:24:42 +0100

    Create footer code box.php

---
+++ footer code box.php
@@ -0,0 +1,34 @@
+<footer>
+<ul>
+<li>
+<p class="home">Home</p>
+<a class="logo" href="http://www.myweeblytricks.com">MY WEEBLY TRICKS<i>© 2014</i></a>
+</li>
+<li>
+<p class="services">Services</p>
+ <ul>
+  <li><a href="#">Site Editing</a></li>
+  <li><a href="#">Online Tutorial</a></li>
+  <li><a href="#">Mobile development</a></li>
+  <li><a href="#">Web &amp; Print Design</a></li>
+ </ul>
+</li>
+<li>
+<p class="reachus">Reach us</p>
+ <ul>
+  <li><a href="#">Email</a></li>
+  <li><a href="#">Twitter</a></li>
+  <li><a href="#">Facebook</a></li>
+  <li>555-123456789</li>
+ </ul>
+</li>
+<li>
+<p class="clients">Clients</p>
+<ul>
+  <li><a href="#">Login Area</a></li>
+  <li><a href="#">Support Center</a></li>
+  <li><a href="#">FAQ</a></li>
+</ul>
+</li>
+  </ul>
+</footer>
