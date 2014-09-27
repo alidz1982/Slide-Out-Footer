4b83199a0d1897219ee6294fa0a782235cbe4ff9
alidz1982 <alidz1982@users.noreply.github.com>
2014-09-27 19:31:46 +0100

    Update Header Code.php

--- Header Code.php
+++ Header Code.php
@@ -1,5 +1,5 @@
 <link href="http://fonts.googleapis.com/css?family=PT+Sans+Narrow:700" rel="stylesheet">
-  <link href="http://demo.tutorialzine.com/2013/08/slideout-footer-css/assets/css/style.css" rel="stylesheet">
+  <link href="https://github.com/alidz1982/Slide-Out-Footer/blob/master/style.css" rel="stylesheet">
     <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <link rel="stylesheet" type="text/css" href="https://github.com/alidz1982/Slide-Out-Footer/blob/master/slideoutfooter.css">
     <style>
